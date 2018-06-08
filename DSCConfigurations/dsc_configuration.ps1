aae1fc73703891a1cd6de35f28260a434e1a62be
Adam Birds <adam.birds@adbtechltd.co.uk>
2018-06-08 18:13:40 +0000

    Update version to $newVersion
    Signed-off-by: Adam Birds <adam.birds@adbtechltd.co.uk>

--- DSCConfigurations/dsc_configuration.ps1
+++ DSCConfigurations/dsc_configuration.ps1
@@ -2,7 +2,7 @@ Configuration Hubot {
 
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -Name MSFT_xRemoteFile -ModuleName xPSDesiredStateConfiguration
-    Import-DscResource -ModuleName @{ModuleName="InstallHubot"; RequiredVersion="2.0.51"}
+    Import-DscResource -ModuleName @{ModuleName="InstallHubot"; RequiredVersion="2.0.52"}
 
     node $AllNodes.Where{$_.Role -eq "Hubot"}.NodeName {         
         # Set an adapter for hubot to use
