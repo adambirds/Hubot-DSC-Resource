36def8f15a32f545bed77ef517a35f4a2124babe
Adam Birds <adam.birds@adbtechltd.co.uk>
2018-06-08 18:41:01 +0000

    Update version to $($env:newVersion)
    Signed-off-by: Adam Birds <adam.birds@adbtechltd.co.uk>

--- DSCConfigurations/dsc_configuration.ps1
+++ DSCConfigurations/dsc_configuration.ps1
@@ -2,7 +2,7 @@ Configuration Hubot {
 
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -Name MSFT_xRemoteFile -ModuleName xPSDesiredStateConfiguration
-    Import-DscResource -ModuleName @{ModuleName="InstallHubot"; RequiredVersion="2.0.52"}
+    Import-DscResource -ModuleName @{ModuleName="InstallHubot"; RequiredVersion="2.0.53"}
 
     node $AllNodes.Where{$_.Role -eq "Hubot"}.NodeName {         
         # Set an adapter for hubot to use
--- InstallHubot/InstallHubot.psd1
+++ InstallHubot/InstallHubot.psd1
@@ -12,7 +12,7 @@
 RootModule = 'InstallHubot.psm1'
 
 # Version number of this module.
-ModuleVersion = '2.0.52'
+ModuleVersion = '2.0.53'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
